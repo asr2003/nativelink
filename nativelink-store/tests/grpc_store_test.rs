50a6c709ed44a68bd3eefec81669dfd15132726a
Ambati Sahithi <162500856+asr2003@users.noreply.github.com>
2025-05-26 02:22:36 +0530

    fmt

--- nativelink-store/tests/grpc_store_test.rs
+++ nativelink-store/tests/grpc_store_test.rs
@@ -25,7 +25,8 @@ fn test_is_supported_digest_function() {
 
 #[test]
 fn test_read_rejects_invalid_digest_function() -> Result<(), Box<dyn core::error::Error>> {
-    const RESOURCE_NAME: &str = "instance_name/blobs/crc32/hash/12345";
+    const RESOURCE_NAME: &str =
+        "instance/blobs/crc32/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/123";
     let resource_info = ResourceInfo::new(RESOURCE_NAME, false)?;
     let digest_func = resource_info
         .digest_function
